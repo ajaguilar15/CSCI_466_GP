db2f94d62842bfb6efcc31b225343a3ea9c6df72
op6123 <patelom994@gmail.com>
2025-04-22 23:10:06 -0500

    Create accounts.php
    Sign in should show a welcome back but the create account isn't working yet.

---
+++ accounts.php
@@ -0,0 +1,88 @@
+<html>
+  <body>
+    <h1>Welcome to our Best Buy Store</h1>
+    <h2>Do you want to sign in or create your own account?</h2>
+    <?php
+      $dsn = "mysql:host=courses;dbname=z2043569";
+      $pdo = new PDO($dsn, "z2043569", "2004Apr07");
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $action = $_POST['action'];
+        switch ($action) {
+          case 'signin': 
+            if (isset($_POST['email']) && isset($_POST['pwd'])) {
+              $stmt = $pdo->prepare("SELECT * FROM USERS WHERE EMAIL = ? AND PASSWORD = ?");
+              $stmt->execute([$_POST['email'], $_POST['pwd']]);
+              $user = $stmt->fetch(PDO::FETCH_ASSOC);
+              if ($user) {
+                echo "<h2>Welcome back, {$user['FIRST_NAME']}!</h2>";
+              }
+              else {
+                echo "<h2>Invalid email or password!</h2>";
+              }
+            }
+            else {
+              echo "<h2>Type in your Email and Password</h2>";
+              echo "<form method='POST' action=''>";
+              echo "<input type='hidden' name='action' value='signin'>";
+              echo "<label for='email'>Email:</label><br>";
+              echo "<input type='text' name='email' id='email' required><br><br>";
+              echo "<label for='pwd'>Password:</label><br>";
+              echo "<input type='password' name='pwd' id='pwd' required><br><br>";
+              echo "<input type='submit' value='Submit'>";
+              echo "</form>";
+            }
+            break;
+          case 'create_account':
+            if (isset($_POST['cr_email']) && isset($_POST['cr_pwd']) && isset($_POST['first']) && isset($_POST['last']) && isset($_POST['phn'])) {
+              $stmt = $pdo->prepare("INSERT INTO USER (USERID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ADDRESS, PHONENUM, BILLINFO) VALUES (
+?, ?, ?, ?, ?, ?, ?, ?)");
+              $stmt->execute([$_POST['uid'], $_POST['first'], $_POST['last'], $_POST['cr_email'], $_POST['cr_pwd'], $_POST['Address'], $_POST['phn'], $_POST['bill']]);
+              $user = $stmt->fetch(PDO::FETCH_ASSOC);
+              if ($user) {
+                echo "<h2>Welcome, {$user['FIRST_NAME']}!</h2>";
+              }
+              else {
+                echo "<h2>You failed to put in one of the requirements</h2>";
+              }
+            }
+            else {
+              echo "<h2>Create Your Account</h2>";
+              echo "<form method='POST' action=''>";
+              echo "<label for='first'>First Name:</label><br>";
+              echo "<input type='text' name='first' id='first'><br><br>";
+              echo "<label for='last'>Last Name:</label><br>";
+              echo "<input type='text' name='last' id='last'><br><br>";
+              echo "<label for='cr_email'>Email:</label><br>";
+              echo "<input type='text' name='cr_email' id='cr_email'><br><br>";
+              echo "<label for='cr_pwd'>Password:</label><br>";
+              echo "<input type='password' name='cr_pwd' id='cr_pwd'><br><br>";
+              echo "<label for='Address'>Address:</label><br>";
+              echo "<input type='text' name='address' id='address'><br><br>";
+              echo "<label for='cr_uid'>UserID:</label><br>";
+              echo "<input type='text' name='cr_uid' id='cr_uid'><br><br>";
+              echo "<label for='phn'>Phone Number:</label><br>";
+              echo "<input type='text' name='phn' id='phn'><br><br>";
+              echo "<label for='bill'>BillingInfo:</label><br>";
+              echo "<input type='text' name='bill' id='bill'><br><br>";
+              echo "<input type='submit' value='Create Account'>";
+              echo "</form>";
+            }
+            break;
+          default:
+            echo " ";
+        }
+      }
+      else {
+        echo "<form method='POST' action=''>";
+        echo "<label for='action'>What would you like to do?</label>";
+        echo "<select name='action' id='action' required>";
+        echo "<option value=''>----Choose an option----</option>";
+        echo "<option value='signin'>Sign In</option>";
+        echo "<option value='create_account'>Create An Account</option>";
+        echo "</select>";
+        echo "<input type='submit' value='Submit'>";
+        echo "</form>";
+      }
+    ?>
+  </body>
+</html>
