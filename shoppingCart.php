87353003c8c3092325a78a7cfc349bb4856e278e
Dondonello <149278609+Dondonello@users.noreply.github.com>
2025-04-27 00:35:46 -0500

    Add files via upload

--- shoppingCart.php
+++ shoppingCart.php
@@ -26,25 +26,68 @@
         //$fname = $name[FIRST_NAME];
         //$lname = $name[LAST_NAME];
 
+        echo "<form method=POST action=https://students.cs.niu.edu/~z2045088/products.php>";
+        echo "<input type=hidden name=email value=$_POST[email]>";
+        echo "<input type=hidden name=pwd value=$_POST[pwd]>";
+        echo "<input type=hidden name=userID value=$_POST[userID]>";
+        echo "<input type=submit value='View Items'>";
+        echo "</form>";
+
         //show user's shopping cart
-    $sel = "SELECT PNAME, PRICE, USERQTY FROM PRODUCT JOIN SHOPPING_CART WHERE PRODUCT.PRODUCTID=SHOPPING_CART.PRODUCTID AND USERID='$_POST[userID]';";
+    $sel = "SELECT SHOPPING_CART.PRODUCTID, PNAME, PRICE, USERQTY FROM PRODUCT JOIN SHOPPING_CART WHERE PRODUCT.PRODUCTID=SHOPPING_CART.PRODUCTID AND USERID='$_POST[userID]';";
         $result = $pdo->query($sel);
         $show = $result->fetchALL(PDO::FETCH_ASSOC);
-        
+        $prodList=$show;
         //get the total for each product aas we display the cart
         $prodTot=0;
         $Total=0;
         echo "<h1>$fname $lname's Cart</h1>";
 
+        //update the quantity amount
+        if($_POST['changeQTY'] != NULL){
+            foreach($prodList as $r){
+                if($r['PRODUCTID'] == $_POST['pchange']){
+                    $pamount=$r['USERQTY'];
+                }
+            }
+
+            //adding or removing
+            if($_POST['change'] == 'Add'){
+                $changeQTY = $pamount + $_POST['changeQTY'];
+            }
+            if($_POST['change'] == 'Remove'){
+                $changeQTY = $pamount - $_POST['changeQTY'];
+            }
+
+            //if less than zero then remove item from list
+            if($changeQTY < 1){
+                $sel = "DELETE FROM SHOPPING_CART WHERE USERID='$_POST[userID]' AND PRODUCTID='$_POST[pchange]';";
+            }
+            else{
+                $sel = "UPDATE SHOPPING_CART SET USERQTY='$changeQTY' WHERE USERID='$_POST[userID]' AND PRODUCTID='$_POST[pchange]';";
+            }
+            $result = $pdo->prepare($sel);
+            $succ = $result->execute();
+
+            //update prodList
+            $sel = "SELECT SHOPPING_CART.PRODUCTID, PNAME, PRICE, USERQTY FROM PRODUCT JOIN SHOPPING_CART WHERE PRODUCT.PRODUCTID=SHOPPING_CART.PRODUCTID AND USERID='$_POST[userID]';";
+            $result = $pdo->query($sel);
+            $show = $result->fetchALL(PDO::FETCH_ASSOC);
+            $prodList=$show;
+        }
+
+
         //table displaying cart
         echo "<table border=3><tbody>";
         echo "<tr>";
+        echo "<th>Product ID</th>";
         echo "<th>Product Name</th>";
         echo "<th>Price</th>";
         echo "<th>Quantity</th>";
         echo "<th>Product Total</th>";
         echo "</tr>";
-        foreach($show as $r){
+
+        foreach($prodList as $r){
             echo "<tr>";
             foreach($r as $c){
                 echo "<th>$c</th>";
@@ -57,6 +100,28 @@
         }
         echo "</tbody></table>";
 
+        //allow user to change quantity of a product
+        echo "<br>";
+        echo "Change the quantity of a product";
+        echo "<br>";
+        echo "<form method=POST action=>";
+        echo "<select name=pchange>";
+        foreach($prodList as $r){
+            echo "<option value=$r[PRODUCTID]>$r[PRODUCTID]</option>";
+        }
+        echo "/<select>";
+        echo "<select name=change>";
+        echo "<option value=Add>Add</option>";
+        echo "<option value=Remove>Remove</option>";
+        echo "</select>";
+        echo "<input type=number name=changeQTY min=1>";        
+        echo "<input type=hidden name=email value=$_POST[email]>";
+        echo "<input type=hidden name=pwd value=$_POST[pwd]>";
+        echo "<input type=hidden name=userID value=$_POST[userID]>";
+        echo "<input type=submit value='Change QTY'>";
+        echo "</form>";
+
+
         //see total and purchase
         echo "<form method=POST action=>";
         echo "Your total is $$Total ";
@@ -66,7 +131,7 @@
         echo "<input type=hidden name=userID value=$_POST[userID]>";
         echo "</form>";
 
-        //add code to add billing info and finalize purchase and whatnot
+        //add code to add billing info and finalize purchase and empty cart etc, may need date, whatnot
      ?>
     </body>
 </html>
