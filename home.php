5dbe2b3b641deaf5da1e2ec8bdd80abd7befb9f0
op6123 <patelom994@gmail.com>
2025-04-29 16:41:05 -0500

    Update home.php
    User gets a random generated userid when they create an account

--- home.php
+++ home.php
@@ -8,15 +8,27 @@ try { //Tries to login to mariadb and checks if works
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-if (isset($_POST["cr_uid"]) && isset($_POST["first"]) && isset($_POST["last"]) && isset($_POST["cr_email"]) && isset($_POST["cr_pwd"])
-&& isset($_POST["address"]) && isset($_POST["phn"]) && isset($_POST["bill"])) {
+$n = 10;
+function getRandomChar($n) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $ranChar = '';
+    for ($i = 0; $i < $n; $i++) {
+        $index = random_int(0, strlen($characters) - 1);
+        $ranChar .= $characters[$index];
+    }
+
+    return $ranChar;
+}
+if (isset($_POST["first"]) && isset($_POST["last"]) && isset($_POST["cr_email"]) && isset($_POST["cr_pwd"]) && isset($_POST["address"])
+&& isset($_POST["phn"]) && isset($_POST["bill"])) {
+    $ranChar = getRandomChar($n);
     $stmt = $pdo->prepare("INSERT INTO USERS (USERID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, ADDRESS, PHONENUM, BILLINFO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$_POST['cr_uid'], $_POST['first'],  $_POST['last'],  $_POST['cr_email'],  $_POST['cr_pwd'],  $_POST['address'],  $_POST['phn'],  $_POST['bill']]);
+    $stmt->execute([$ranChar, $_POST['first'],  $_POST['last'],  $_POST['cr_email'],  $_POST['cr_pwd'],  $_POST['address'],  $_POST['phn'],  $_POST['bill']]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($stmt->rowCount()) {
       echo "<h2>Welcome, {$_POST['first']}!</h2>";
       echo "<h2>Click here to go back to signin: </h2>";
-      echo "<a href='https://students.cs.niu.edu/~z2045088/accounts.php'>Here</a>";
+      echo "<a href='https://students.cs.niu.edu/~z2043569/acc.php'>Here</a>";
      }
     else {
       echo "<h2>You failed at least one of the requirements!</h2>";
